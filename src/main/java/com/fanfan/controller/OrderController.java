b5f4e319e6ed7d9578f0c4cb281b899e5f1d1473
fanrujiang <1066643487@qq.com>
2023-02-24 19:06:41 +0800

    完成 用户订单查询

--- src/main/java/com/fanfan/controller/OrderController.java
+++ src/main/java/com/fanfan/controller/OrderController.java
@@ -1,12 +1,22 @@
 package com.fanfan.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fanfan.bean.AddressBook;
+import com.fanfan.bean.OrderDetail;
 import com.fanfan.bean.Orders;
+import com.fanfan.bean.User;
 import com.fanfan.common.R;
+import com.fanfan.dto.OrdersDto;
+import com.fanfan.service.AddressBookService;
+import com.fanfan.service.OrderDetailService;
 import com.fanfan.service.OrderService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fanfan.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -15,10 +25,17 @@ public class OrderController {
 
 
     private final OrderService orderService;
+    private final OrderDetailService orderDetailService;
+    private final UserService userService;
+    private final AddressBookService addressBookService;
 
-    public OrderController(OrderService orderService) {
+
+    public OrderController(OrderService orderService, OrderDetailService orderDetailService, UserService userService, AddressBookService addressBookService) {
         this.orderService = orderService;
 
+        this.orderDetailService = orderDetailService;
+        this.userService = userService;
+        this.addressBookService = addressBookService;
     }
 
     /**
@@ -31,5 +48,48 @@ public class OrderController {
     public R<String> submit(@RequestBody Orders orders) {
         orderService.submitl(orders);
         return R.success("下单成功");
+    }
+
+
+    @GetMapping("userPage")
+    public R<Page> page (int page, int pageSize){
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        Page<OrdersDto> pageDto = new Page<>();
+
+        orderService.page(pageInfo);
+        // 将其除了records中的内存复制到pageDto中
+        BeanUtils.copyProperties(pageInfo, pageDto, "records");
+
+        List<Orders> records = pageInfo.getRecords();
+
+        List<OrdersDto> collect = records.stream().map((order) -> {
+            OrdersDto ordersDto = new OrdersDto();
+
+            BeanUtils.copyProperties(order, ordersDto);
+            // 根据订单id查询订单详细信息
+
+            QueryWrapper<OrderDetail> wrapperDetail = new QueryWrapper<>();
+            wrapperDetail.eq("order_id", order.getId());
+
+            List<OrderDetail> orderDetails = orderDetailService.list(wrapperDetail);
+            ordersDto.setOrderDetails(orderDetails);
+
+            // 根据userId 查询用户姓名
+            Long userId = order.getUserId();
+            User user = userService.getById(userId);
+            ordersDto.setUserName(user.getName());
+            ordersDto.setPhone(user.getPhone());
+
+            // 获取地址信息
+            Long addressBookId = order.getAddressBookId();
+            AddressBook addressBook = addressBookService.getById(addressBookId);
+            ordersDto.setAddress(addressBook.getDetail());
+            ordersDto.setConsignee(addressBook.getConsignee());
+
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        pageDto.setRecords(collect);
+        return R.success(pageDto);
     }
 }
